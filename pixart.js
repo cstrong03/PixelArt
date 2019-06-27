2952d7291f33af3ea34ff16b5c96d25591a96148
Charisse Strong <msstrong92@gmail.com>
2019-06-27 18:44:47 -0400

    pixel art

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
-// This is all the magic
+let box = document.querySelector('.brush');
+let field = document.querySelector('#color-field');
+
+const setColor = (evt)=>{
+box.style.backgroundColor = field.value;
+evt.preventDefault();
+}
+
+let s = document.querySelector('#set-color');
+s.addEventListener('click', setColor)
+
+let color = document.querySelectorAll('body');
+
+const change = (evt)=>{
+		evt.target.style.backgroundColor = field.value;
+	}
+
+for (let i = 0; i <= 8000; i++) {
+	let kitty = document.createElement('div');
+	kitty.addEventListener('mouseover', change);
+	document.body.appendChild(kitty).className = 'square';
+}
